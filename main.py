2e7d799d6f14e6b07b343cffbe10f60e4ebaa07f
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-03-10 10:14:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from tornado.web import Application, RequestHandler, url
+import tornado.ioloop
+
+class Boilerplate(RequestHandler):
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', "*")
+
+class HomeHandler(Boilerplate):
+    def get(self):
+        pass
+
+class CreateGameHandler(Boilerplate):
+    def get(self): 
+        pass
+
+class JoinGameHandler(Boilerplate):
+    def get(self):
+        pass
+
+
+def init():
+    return Application([
+        url(r'/', HomeHandler),
+        url(r'/creategame', CreateGameHandler),
+        url(r'/joingame', JoinGameHandler)
+    ])
+
+app = init()
+app.listen(8000)
+tornado.ioloop.IOLoop.current().start()
